278206123a39dcd87b44ef4232948c6ee84cfcd3
nivpc <niv.goldenberg@gmail.com>
2019-04-27 21:57:59 +0300

    Fix more style issues (import should be order Alphabetical) in the Avoid debug statement in loops rule.

--- pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/performance/AvoidDebugStatementsInLoopsRule.java
+++ pmd-apex/src/main/java/net/sourceforge/pmd/lang/apex/rule/performance/AvoidDebugStatementsInLoopsRule.java
@@ -1,11 +1,15 @@
+/**
+ * BSD-style license; for more info see http://pmd.sourceforge.net/license.html
+ */
+
 package net.sourceforge.pmd.lang.apex.rule.performance;
 
-import net.sourceforge.pmd.lang.apex.ast.ASTMethodCallExpression;
-import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
 import net.sourceforge.pmd.lang.apex.ast.ASTDoLoopStatement;
-import net.sourceforge.pmd.lang.apex.ast.ASTWhileLoopStatement;
-import net.sourceforge.pmd.lang.apex.ast.ASTForLoopStatement;
 import net.sourceforge.pmd.lang.apex.ast.ASTForEachStatement;
+import net.sourceforge.pmd.lang.apex.ast.ASTForLoopStatement;
+import net.sourceforge.pmd.lang.apex.ast.ASTMethodCallExpression;
+import net.sourceforge.pmd.lang.apex.ast.ASTWhileLoopStatement;
+import net.sourceforge.pmd.lang.apex.rule.AbstractApexRule;
 import net.sourceforge.pmd.lang.ast.AbstractNode;
 import net.sourceforge.pmd.lang.ast.Node;
 
